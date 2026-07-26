4d131e1f8d4326131686235e093a9b567707075e
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-07-27 01:01:38 +0900

    04

--- main.c
+++ main.c
@@ -8,10 +8,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char *argv[]) {
 int a;
-	printf(" input the second : ");
+	printf("input the year : ");
 	scanf("%d", &a);
-	printf(" the time is %d : %d", a/60, a%60);
-
+	printf(" is the year %d the leap year? : %d\n",a, (a%4 == 0 && a%100!=0) || a%400==0);
 	return 0;
 }
 

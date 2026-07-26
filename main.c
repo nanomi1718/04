be54301fb206d6adbd8bb4dd35ea174bfe181d9d
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-07-27 00:58:26 +0900

    03

--- main.c
+++ main.c
@@ -6,14 +6,12 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char *argv[]) {
+int a;
+	printf(" input the second : ");
+	scanf("%d", &a);
+	printf(" the time is %d : %d", a/60, a%60);
 
-int a, b;
-printf("input two integers: ");
-scanf("%d, %d", &a, &b);
-printf(" + result is %d\n", a+b);
-printf(" - result is %d\n", a-b);
-printf(" * result is %d\n", a*b);
-printf(" / result is %d\n", a/b);
-printf(" %% result is %d\n", a%b);
 	return 0;
 }
+
